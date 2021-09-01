3a5c3968c48299d3741d10601869d2736ae5885e
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2021-09-01 14:29:13 +0300

    Eski komutlardan kurtulduk

--- commands/other/undefined.js
+++
@@ -1,9 +0,0 @@
-exports.conf = {
-        aliases: ['p', 'pong', 'uptime',],
-        permLevel: 0
-};
-exports.help = {
-               name: '-',
-               description: 'Böyle bir komut yok!',
-               usage: 'Lütden botta bulunan bir komutu gir.'
- };
