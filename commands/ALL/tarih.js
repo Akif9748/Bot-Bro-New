73d30e0ebd05ff6cba7754e8eb1297e9be32534a
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2021-10-31 19:50:21 +0300

    Delete commands/ALL directory

--- commands/ALL/tarih.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-var zaman = new Date();
-message.channel.send(zaman.toLocaleString('tr-TR', { timeZone: 'Turkey' }));
-};
-exports.help = {
-name: 'tarih',
-description: 'Tarih ve saati yollar.',
-usage: 'tarih'
-};
